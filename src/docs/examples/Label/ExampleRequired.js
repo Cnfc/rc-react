d5f3f27ba667915cafd91e390c1d629d1c641dd0
Cnfc <stdashkov@mail.ru>
2019-10-21 18:19:55 +0300

    Label

---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import Label from 'ps-react/Label';
+
+
+/** Optional label */
+function ExampleHelloWorld() {
+  return <Label htmlFor='test' label="text" required />
+}
+export default ExampleHelloWorld;
