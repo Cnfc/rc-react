08260ea7ef8d206e90e2731711fc0e3b93f3811b
Cnfc <stdashkov@mail.ru>
2019-10-22 19:19:51 +0300

    add final submin v

--- src/docs/examples/TextInput/ExampleOptional.js
+++ src/docs/examples/TextInput/ExampleOptional.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import TextInput from 'ps-react/TextInput';
+import TextInput from 'reusable-react-lib/TextInput';
 
 /** Optional TextBox */
 export default class ExampleOptional extends React.Component {
